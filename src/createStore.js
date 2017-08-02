591ecee6a1f46970b336bf22ea98d40a7547025f
Brett Schules <brettschules@gmail.com>
2017-08-02 18:35:24 -0400

    Done

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,20 @@
+import {render} from './index'
+export default function
+  createStore(reducer) {
+    let state;
 
+    function dispatch(action) {
+      state = reducer(action, state)
+      console.log(`the state is ${state.count}`)
+      render()
+    }
+
+    function getState() {
+      return state
+    }
+
+    return {
+      dispatch,
+      state
+    }
+  }
